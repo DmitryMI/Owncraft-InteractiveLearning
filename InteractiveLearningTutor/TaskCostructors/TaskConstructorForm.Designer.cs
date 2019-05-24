59bba4d83d6f993973c4bbccd7c52fcf6c6eb11d
DmitryMI <wet75787@tqosi.com>
2019-05-24 23:00:39 +0300

    Removed unused tab

--- InteractiveLearningTutor/TaskCostructors/TaskConstructorForm.Designer.cs
+++ InteractiveLearningTutor/TaskCostructors/TaskConstructorForm.Designer.cs
@@ -30,14 +30,13 @@
         {
             this.TabControl = new System.Windows.Forms.TabControl();
             this.SimpleTaskTab = new System.Windows.Forms.TabPage();
-            this.RandomTaskTab = new System.Windows.Forms.TabPage();
-            this.FinishButton = new System.Windows.Forms.Button();
-            this.simpleNameBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.simpleTextBox = new System.Windows.Forms.RichTextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.simpleAnswerBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.simpleTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.simpleNameBox = new System.Windows.Forms.TextBox();
+            this.FinishButton = new System.Windows.Forms.Button();
             this.TabControl.SuspendLayout();
             this.SimpleTaskTab.SuspendLayout();
             this.SuspendLayout();
@@ -45,7 +44,6 @@
             // TabControl
             // 
             this.TabControl.Controls.Add(this.SimpleTaskTab);
-            this.TabControl.Controls.Add(this.RandomTaskTab);
             this.TabControl.Location = new System.Drawing.Point(12, 12);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
@@ -68,49 +66,21 @@
             this.SimpleTaskTab.Text = "Simple";
             this.SimpleTaskTab.UseVisualStyleBackColor = true;
             // 
-            // RandomTaskTab
+            // label3
             // 
-            this.RandomTaskTab.Location = new System.Drawing.Point(4, 22);
-            this.RandomTaskTab.Name = "RandomTaskTab";
-            this.RandomTaskTab.Padding = new System.Windows.Forms.Padding(3);
-            this.RandomTaskTab.Size = new System.Drawing.Size(637, 355);
-            this.RandomTaskTab.TabIndex = 1;
-            this.RandomTaskTab.Text = "Random equation";
-            this.RandomTaskTab.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ответ:";
             // 
-            // FinishButton
+            // simpleAnswerBox
             // 
-            this.FinishButton.Location = new System.Drawing.Point(578, 399);
-            this.FinishButton.Name = "FinishButton";
-            this.FinishButton.Size = new System.Drawing.Size(75, 23);
-            this.FinishButton.TabIndex = 0;
-            this.FinishButton.Text = "Сохранить";
-            this.FinishButton.UseVisualStyleBackColor = true;
-            this.FinishButton.Click += new System.EventHandler(this.FinishButton_Click);
-            // 
-            // simpleNameBox
-            // 
-            this.simpleNameBox.Location = new System.Drawing.Point(69, 14);
-            this.simpleNameBox.Name = "simpleNameBox";
-            this.simpleNameBox.Size = new System.Drawing.Size(562, 20);
-            this.simpleNameBox.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Название:";
-            // 
-            // simpleTextBox
-            // 
-            this.simpleTextBox.Location = new System.Drawing.Point(69, 40);
-            this.simpleTextBox.Name = "simpleTextBox";
-            this.simpleTextBox.Size = new System.Drawing.Size(562, 268);
-            this.simpleTextBox.TabIndex = 2;
-            this.simpleTextBox.Text = "";
+            this.simpleAnswerBox.Location = new System.Drawing.Point(69, 329);
+            this.simpleAnswerBox.Name = "simpleAnswerBox";
+            this.simpleAnswerBox.Size = new System.Drawing.Size(562, 20);
+            this.simpleAnswerBox.TabIndex = 4;
             // 
             // label2
             // 
@@ -121,21 +91,39 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Текст:";
             // 
-            // label3
+            // simpleTextBox
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 332);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Ответ:";
+            this.simpleTextBox.Location = new System.Drawing.Point(69, 40);
+            this.simpleTextBox.Name = "simpleTextBox";
+            this.simpleTextBox.Size = new System.Drawing.Size(562, 268);
+            this.simpleTextBox.TabIndex = 2;
+            this.simpleTextBox.Text = "";
             // 
-            // SimpleAnswerBox
+            // label1
             // 
-            this.simpleAnswerBox.Location = new System.Drawing.Point(69, 329);
-            this.simpleAnswerBox.Name = "simpleAnswerBox";
-            this.simpleAnswerBox.Size = new System.Drawing.Size(562, 20);
-            this.simpleAnswerBox.TabIndex = 4;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название:";
+            // 
+            // simpleNameBox
+            // 
+            this.simpleNameBox.Location = new System.Drawing.Point(69, 14);
+            this.simpleNameBox.Name = "simpleNameBox";
+            this.simpleNameBox.Size = new System.Drawing.Size(562, 20);
+            this.simpleNameBox.TabIndex = 0;
+            // 
+            // FinishButton
+            // 
+            this.FinishButton.Location = new System.Drawing.Point(578, 399);
+            this.FinishButton.Name = "FinishButton";
+            this.FinishButton.Size = new System.Drawing.Size(75, 23);
+            this.FinishButton.TabIndex = 0;
+            this.FinishButton.Text = "Сохранить";
+            this.FinishButton.UseVisualStyleBackColor = true;
+            this.FinishButton.Click += new System.EventHandler(this.FinishButton_Click);
             // 
             // TaskConstructorForm
             // 
@@ -158,7 +146,6 @@
 
         private System.Windows.Forms.TabControl TabControl;
         private System.Windows.Forms.TabPage SimpleTaskTab;
-        private System.Windows.Forms.TabPage RandomTaskTab;
         private System.Windows.Forms.Button FinishButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox simpleAnswerBox;
